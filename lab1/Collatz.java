194eb6401096e6aa0dcb387ef9d9ef8b92628e8e
Administrator <657136463@qq.com>
2025-02-10 21:29:24 +0800

    completed lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,27 +1,21 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
- */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Correct implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
+        if (n % 2 == 0) { // 如果是偶数
             return n / 2;
+        } else {          // 如果是奇数
+            return 3 * n + 1;
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; // 可以更改为任何正整数
         System.out.print(n + " ");
-        while (n != 1) {
+        while (n != 1) { // Collatz 猜想的循环条件
             n = nextNumber(n);
             System.out.print(n + " ");
         }
         System.out.println();
     }
 }
-
